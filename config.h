bc7dc570049d2e2d17db8a4ab2abdf51637361c3
mrxir <zicongxie@foxmail.com>
2022-01-06 14:36:05 +0800

    update unkown but not wrong config.h

--- config.h
+++ config.h
@@ -142,6 +142,8 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_6,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_6,      tag,            {.ui = ~0 } },
+	// { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+	// { MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
